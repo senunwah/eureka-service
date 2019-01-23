53c1730dbf313fb2d5bdac1e02e16270c073c4e0
Enunwah Stephen <senunwah@yahoo.com>
2019-01-23 15:05:01 +0100

    Initial commit

---
+++ src/main/java/com/example/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//Data Annotation to Enable the eureka server in the Spring Boot Application Entry point
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
+
